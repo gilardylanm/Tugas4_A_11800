7bda39bb86de94a292efbc0d7ef54d4e29d308cf
gilardylanm <220711800@students.uajy.ac.id>
2024-11-07 16:00:02 +0700

    Update Tugas_Modul4_A_DYLAN_11800.py

--- Tugas_Modul4_A_DYLAN_11800.py
+++ Tugas_Modul4_A_DYLAN_11800.py
@@ -47,11 +47,11 @@ if uploaded_file is not None:
     st.markdown("<h1 style='text-align: center;'>Unsupervised Learning - Dylan</h1>", unsafe_allow_html=True)
     st.dataframe(input_data)
 
-    model_directory = r'C:\Users\ROG STRIX\Documents\000000000000000 MESIN\Tugas4_A_11800'
+    
     model_path = {
-        "AGG_model" : os.path.join(model_directory, r'AGG_model.pkl'),
-        "KMeans_model" : os.path.join(model_directory, r'KMeans_model.pkl'),
-        "DBSCAN_model" : os.path.join(model_directory, r'DBSCAN_model.pkl'),
+        "AGG_model" : os.path(r'AGG_model.pkl'),
+        "KMeans_model" : os.path(r'KMeans_model.pkl'),
+        "DBSCAN_model" : os.path(r'DBSCAN_model.pkl'),
     }
 
     models = {}
